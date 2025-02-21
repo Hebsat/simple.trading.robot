0c9b6ac4bf327f80573a354b2713f0e68c02f546
Yuri Kaleganov <yurkaleganov@yandex.ru>
2025-02-21 21:37:15 +0300

    init

---
+++ src/main/java/com/simple/trading/robot/SimpleTradingRobotApplication.java
@@ -0,0 +1,13 @@
+package com.simple.trading.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTradingRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTradingRobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simple/trading/robot/SimpleTradingRobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.trading.robot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTradingRobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
